c3693963f56304c81592ce7c73bce1999e6f3ec1
fpapado <fotis@futurice.com>
2019-02-21 09:34:01 +0200

    reset

--- dist/index.mjs
+++
@@ -1,2 +0,0 @@
-import"css-paint-polyfill";CSS.paintWorklet.addModule("/dist/paint.js").then(function(t){document.querySelector("html").classList.add("is-loaded")}).catch(function(t){return console.error("Unable to register paintWorklet",t)});
-//# sourceMappingURL=index.mjs.map
--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
-import "css-paint-polyfill";
-
 function init() {
-  CSS.paintWorklet
-    .addModule("/dist/paint.js")
-    .then(_ok => addLoaded())
-    .catch(err => console.error("Unable to register paintWorklet", err));
+  if ('paintWorklet' in CSS) {
+    CSS.paintWorklet
+      .addModule('/dist/paint.js')
+      .then(_ok => addLoaded())
+      .catch(err => console.error('Unable to register paintWorklet', err));
+  } else {
+    console.log('paintWorklet not supported, falling back');
+  }
 }
 
 function addLoaded() {
-  document.querySelector("html").classList.add("is-loaded");
+  document.querySelector('html').classList.add('is-loaded');
 }
 
 init();
